b8b9de7fbb13519a82cf3103c489ac3587a3111c
Loganathan <loganathan.v@congruentglobal.com>
2019-08-14 19:37:48 +0530

    download pdf button added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild('content') content: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  public downloadPdf() {
+    const data = document.getElementById('content');
+    html2canvas(data).then(canvas => {
+    const imgWidth = 208;
+    const pageHeight = 295;
+    const imgHeight = canvas.height * imgWidth / canvas.width;
+    const heightLeft = imgHeight;
+    const contentDataURL = canvas.toDataURL('image/png');
+    const pdf = new jspdf('p', 'mm', 'a4'); 
+    const position = 0;
+    pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+    pdf.save('Deluxe_Branded_Marketing.pdf'); 
+    });
+    }
 
 }
